6d2b4a7747c1b9a9c9be81bab67093632fc820d4
toyoda_k <tennis0810kauppi@gmail.com>
2021-09-25 01:50:45 +0900

    カウントダウンタイマーを実装

--- lib/page/barometer.dart
+++ lib/page/barometer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:quiver/async.dart';
 
 import 'package:environment_sensors/environment_sensors.dart';
 
@@ -14,15 +15,38 @@ class _BarometerState extends State<Barometer> {
   var _time = List<DateTime>.empty();
   final environmentSensors = EnvironmentSensors();
 
+  int _start = 10;
+  int _current = 10;
+
+  void _startTimer() {
+    CountdownTimer countDownTimer = new CountdownTimer(
+      new Duration(seconds: _start), //初期値
+      new Duration(seconds: 1), // 減らす幅
+    );
+
+    var sub = countDownTimer.listen(null);
+    sub.onData((duration) {
+      setState(() {
+        _current = _start - duration.elapsed.inSeconds; //毎秒減らしていく
+      });
+    });
+
+    sub.onDone(() {
+      print("Done");
+      sub.cancel();
+      environmentSensors.pressure.listen((pressure) {
+        print(pressure.toString());
+        _pressures.add(pressure);
+        _time.add(DateTime.now());
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    environmentSensors.pressure.listen((pressure) {
-      print(pressure.toString());
-      _pressures.add(pressure);
-      _time.add(DateTime.now());
-    });
     initPlatformState();
+    _startTimer();
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -30,7 +54,7 @@ class _BarometerState extends State<Barometer> {
     bool pressureAvailable;
 
     pressureAvailable =
-    await environmentSensors.getSensorAvailable(SensorType.Pressure);
+        await environmentSensors.getSensorAvailable(SensorType.Pressure);
 
     setState(() {
       _pressureAvailable = pressureAvailable;
@@ -46,7 +70,11 @@ class _BarometerState extends State<Barometer> {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            leading: IconButton(onPressed: () {Navigator.of(context).pop();}, icon: Icon(Icons.arrow_back)),
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: Icon(Icons.arrow_back)),
             title: Text('トレーニングモード'),
             // 右側のアイコン一覧
             actions: <Widget>[
@@ -56,18 +84,41 @@ class _BarometerState extends State<Barometer> {
               ),
             ],
           ),
-          body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            (_pressureAvailable)
-                ? StreamBuilder<double>(
-                stream: environmentSensors.pressure,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) return CircularProgressIndicator();
-                  return Text(
-                      '圧力: ${snapshot.data?.toStringAsFixed(2)}');
-                })
-                : Text('気圧センサが利用できません。気圧センサが利用できる端末をご利用ください。'),
-            ElevatedButton(onPressed: _finishMeasurement , child: Text('測定終了'))
-          ])),
+          body: Center(
+              child: _current > 0
+                  ? Text("$_current秒",
+                      style: Theme.of(context).textTheme.headline4)
+                  : Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                          (_pressureAvailable)
+                              ? StreamBuilder<double>(
+                                  stream: environmentSensors.pressure,
+                                  builder: (context, snapshot) {
+                                    if (!snapshot.hasData)
+                                      return CircularProgressIndicator();
+                                    return Text(
+                                        '圧力: ${snapshot.data?.toStringAsFixed(2)}');
+                                  })
+                              : Column(children: [
+                                  Text('気圧センサが利用できません。'),
+                                  Text('気圧センサが利用できる端末をご利用ください。')
+                                ]),
+                          SizedBox(height: 50),
+                          Container(
+                              width: 300.0,
+                              height: 50.0,
+                              child: OutlinedButton(
+                                child: const Text('測定終了'),
+                                style: OutlinedButton.styleFrom(
+                                  primary: Colors.black,
+                                  shape: const StadiumBorder(),
+                                  side: const BorderSide(color: Colors.green),
+                                ),
+                                onPressed: _finishMeasurement,
+                              )),
+                        ]))),
     );
   }
 }
