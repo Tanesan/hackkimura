8043403e3390e7635b28c20d0b24e4e6e06870f6
toyoda_k <tennis0810kauppi@gmail.com>
2021-09-25 00:34:20 +0900

    取得した生データを表示

--- lib/page/result.dart
+++ lib/page/result.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
-class Result extends StatelessWidget {
-  // This widget is the root of your application.
+class Result extends StatefulWidget {
+  @override
+  _ResultState createState() => _ResultState();
+}
+
+class _ResultState extends State<Result> {
+  var _pressures = List<double>.empty();
+  var _score;
+  bool _calculated = false;
+
+  int _calculateScore(List<double> pressures) {
+    return 80;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    _pressures = ModalRoute.of(context)?.settings.arguments as List<double>;
+    _score = _calculateScore(_pressures);
+    _calculated = true;
     return Scaffold(
         appBar: AppBar(
-          leading: IconButton(onPressed: () {Navigator.of(context).pop();}, icon: Icon(Icons.arrow_back)),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(Icons.arrow_back)),
           title: Text('トレーニングモード'),
           // 右側のアイコン一覧
           actions: <Widget>[
@@ -16,7 +41,8 @@ class Result extends StatelessWidget {
             ),
           ],
         ),
-        body: Text('結果')
-    );
+        body: _calculated
+            ? Column(children: _pressures.map((pressure) => Text('$pressure, ')).toList())
+            : CircularProgressIndicator());
   }
 }
