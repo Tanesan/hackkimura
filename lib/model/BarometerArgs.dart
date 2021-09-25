75f0dd498cfe9b21dde10878e4cf662b1a9cfc96
toyoda_k <tennis0810kauppi@gmail.com>
2021-09-25 20:17:18 +0900

    時刻の列も表示するようにしました.

--- lib/model/BarometerArgs.dart
+++ lib/model/BarometerArgs.dart
@@ -3,6 +3,7 @@ import 'package:hackkimura/model/UserData.dart';
 class BarometerArgs {
   var userData;
   var pressures;
+  var time;
   var mode;
   BarometerArgs({this.userData, this.pressures, this.mode});
 }
