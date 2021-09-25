8fe424f365fc91547273cc760065491039465373
toyoda_k <tennis0810kauppi@gmail.com>
2021-09-25 20:40:45 +0900

    flutter_barometer_pluginを使った実装

--- lib/page/barometer.dart
+++ lib/page/barometer.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:flutter/services.dart';
 import 'package:quiver/async.dart';
 import 'package:hackkimura/model/BarometerArgs.dart';
-
-import 'package:environment_sensors/environment_sensors.dart';
+import 'package:flutter_barometer_plugin/flutter_barometer.dart';
 
 class Barometer extends StatefulWidget {
   @override
@@ -11,13 +11,12 @@ class Barometer extends StatefulWidget {
 }
 
 class _BarometerState extends State<Barometer> {
-  bool _pressureAvailable = false;
   var _pressures = <double>[];
-  var _time = <DateTime>[];
-  final environmentSensors = EnvironmentSensors();
+  var _time = <int>[];
 
   int _start = 5;
   int _current = 5;
+  var _startTime;
 
   var args = BarometerArgs();
 
@@ -38,12 +37,11 @@ class _BarometerState extends State<Barometer> {
       sub.cancel();
       _pressures.add(0.1);
       _pressures.add(0.2);
-      _time.add(DateTime.now());
-      _time.add(DateTime.now());
-      environmentSensors.pressure.listen((pressure) {
-        print(pressure.toString());
-        _pressures.add(pressure);
-        _time.add(DateTime.now());
+      _time.add(DateTime.now().difference(_startTime).inMilliseconds);
+      _time.add(DateTime.now().difference(_startTime).inMilliseconds);
+      FlutterBarometer.currentPressureEvent.listen((event) {
+        _pressures.add(event.hectpascal.round() / 1000);
+        _time.add(DateTime.now().difference(_startTime).inMilliseconds);
       });
     });
   }
@@ -53,18 +51,25 @@ class _BarometerState extends State<Barometer> {
     super.initState();
     initPlatformState();
     _startTimer();
+    _startTime = DateTime.now();
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    bool pressureAvailable;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
+    String platformVersion;
+    try {
+      platformVersion =
+          await FlutterBarometer.platformVersion ?? 'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
 
-    pressureAvailable =
-    await environmentSensors.getSensorAvailable(SensorType.Pressure);
-
-    setState(() {
-      _pressureAvailable = pressureAvailable;
-    });
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
   }
 
   void _finishMeasurement() {
@@ -75,22 +80,19 @@ class _BarometerState extends State<Barometer> {
 
   @override
   Widget build(BuildContext context) {
-    args = ModalRoute
-        .of(context)
-        ?.settings
-        .arguments as BarometerArgs;
+    args = ModalRoute.of(context)?.settings.arguments as BarometerArgs;
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            leading: IconButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                icon: Icon(Icons.arrow_back)),
-            title: Text(args.mode == "training" ? 'トレーニングモード' : '採点モード'),
-            backgroundColor: Colors.blueGrey[600]
-            // 右側のアイコン一覧
-            /*
+              leading: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: Icon(Icons.arrow_back)),
+              title: Text(args.mode == "training" ? 'トレーニングモード' : '採点モード'),
+              backgroundColor: Colors.blueGrey[600]
+              // 右側のアイコン一覧
+              /*
             actions: <Widget>[
               IconButton(
                 onPressed: () {},
@@ -98,52 +100,33 @@ class _BarometerState extends State<Barometer> {
               ),
             ],
              */
-          ),
+              ),
           body: Center(
               child: _current > 0
-                  ? Column(
-                mainAxisSize: MainAxisSize.min,
-                  children: [Text("測定開始まで",
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .headline4), Text("$_current秒",
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .headline4)
-              ])
-                      : Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    (_pressureAvailable)
-                        ? StreamBuilder<double>(
-                        stream: environmentSensors.pressure,
-                        builder: (context, snapshot) {
-                          if (!snapshot.hasData)
-                            return CircularProgressIndicator();
-                          return Text(
-                              '圧力: ${snapshot.data?.toStringAsFixed(2)}');
-                        })
-                        : Column(children: [
-                      Text('気圧センサが利用できません。'),
-                      Text('気圧センサが利用できる端末をご利用ください。')
-                    ]),
-                    SizedBox(height: 50),
-                    Container(
-                        width: 300.0,
-                        height: 50.0,
-                        child: OutlinedButton(
-                          child: const Text('測定終了'),
-                          style: OutlinedButton.styleFrom(
-                            primary: Colors.black,
-                            shape: const StadiumBorder(),
-                            side: const BorderSide(color: Colors.green),
-                          ),
-                          onPressed: _finishMeasurement,
-                        )),
-                  ]))),
+                  ? Column(mainAxisSize: MainAxisSize.min, children: [
+                      Text("測定開始まで",
+                          style: Theme.of(context).textTheme.headline4),
+                      Text("$_current秒",
+                          style: Theme.of(context).textTheme.headline4)
+                    ])
+                  : Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                          SizedBox(height: 50),
+                          Container(
+                              width: 300.0,
+                              height: 50.0,
+                              child: OutlinedButton(
+                                child: const Text('測定終了'),
+                                style: OutlinedButton.styleFrom(
+                                  primary: Colors.black,
+                                  shape: const StadiumBorder(),
+                                  side: const BorderSide(color: Colors.green),
+                                ),
+                                onPressed: _finishMeasurement,
+                              )),
+                        ]))),
     );
   }
 }
