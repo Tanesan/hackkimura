8aed94eeacbe09ce69d2b44456644bb245a63e5f
toyoda_k <tennis0810kauppi@gmail.com>
2021-10-01 19:40:32 +0900

    マーカーが表示できない...

--- lib/page/AEDMap.dart
+++ lib/page/AEDMap.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 class MyAEDApp extends StatelessWidget {
@@ -25,9 +26,36 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State {
   MapboxMapController? mapController;
+  var _zoomGesturesEnabled = true;
+  var _myLocationEnabled = true;
 
-  void _onMapCreated(MapboxMapController controller) {
+  void _onMapCreated(MapboxMapController controller) async {
     mapController = controller;
+    // Acquire current location (returns the LatLng instance)
+    final result = await acquireCurrentLocation();
+
+    // You can either use the moveCamera or animateCamera, but the former
+    // causes a sudden movement from the initial to 'new' camera position,
+    // while animateCamera gives a smooth animated transition
+    await controller.animateCamera(
+      CameraUpdate.newLatLng(result),
+    );
+
+    // Add a circle denoting current user location
+    await controller.addCircle(
+        CircleOptions(
+          circleRadius: 8.0,
+          circleColor: '#006992',
+          circleOpacity: 0.8,
+
+          // YOU NEED TO PROVIDE THIS FIELD!!!
+          // Otherwise, you'll get a silent exception somewhere in the stack
+          // trace, but the parameter is never marked as @required, so you'll
+          // never know unless you check the stack trace
+          geometry: result,
+          draggable: false,
+        ),
+    );
   }
 
   @override
@@ -37,12 +65,71 @@ class _MyHomePageState extends State {
 
   @override
   Widget build(BuildContext context) {
+    mapController?.addSymbol(
+      SymbolOptions(
+        geometry: LatLng(35.17176088096857, 136.88817886263607),
+        iconImage: "assets/images/AED.jpg",
+      ),
+    );
+
     return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(Icons.arrow_back)),
+          title: Text("AEDマップ"),
+          backgroundColor: Colors.blueGrey[600]
+      ),
+      backgroundColor: Colors.black,
       body: MapboxMap(
+        onMapCreated: _onMapCreated,
+        styleString: MapboxStyles.MAPBOX_STREETS,
+        zoomGesturesEnabled: _zoomGesturesEnabled,
+        myLocationEnabled: _myLocationEnabled,
         initialCameraPosition: const CameraPosition(
             target: LatLng(35.17176088096857, 136.88817886263607),
             zoom: 13.4746),
       ),
     );
   }
+}
+
+
+Future<LatLng> acquireCurrentLocation() async {
+  // Initializes the plugin and starts listening for potential platform events
+  Location location = new Location();
+
+  // Whether or not the location service is enabled
+  bool serviceEnabled;
+
+  // Status of a permission request to use location services
+  PermissionStatus permissionGranted;
+
+  // Check if the location service is enabled, and if not, then request it. In
+  // case the user refuses to do it, return immediately with a null result
+  serviceEnabled = await location.serviceEnabled();
+  if (!serviceEnabled) {
+    serviceEnabled = await location.requestService();
+    if (!serviceEnabled) {
+      return LatLng(0, 0);
+    }
+  }
+
+  // Check for location permissions; similar to the workflow in Android apps,
+  // so check whether the permissions is granted, if not, first you need to
+  // request it, and then read the result of the request, and only proceed if
+  // the permission was granted by the user
+  permissionGranted = await location.hasPermission();
+  if (permissionGranted == PermissionStatus.denied) {
+    permissionGranted = await location.requestPermission();
+    if (permissionGranted != PermissionStatus.granted) {
+      return LatLng(0, 0);
+    }
+  }
+
+  // Gets the current location of the user
+  final locationData = await location.getLocation();
+  return LatLng(locationData.latitude!, locationData.longitude!);
 }
