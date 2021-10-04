dde4f18cc26e2ef86924bf543606a7791ceaa4f6
Keito <54165015+Tanesan@users.noreply.github.com>
2021-10-04 22:07:58 +0900

    [add]top UI change

--- lib/page/play.dart
+++ lib/page/play.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hackkimura/model/BarometerArgs.dart';
 import 'package:hackkimura/model/UserData.dart';
+import 'package:audioplayers/audioplayers.dart' hide PlayerState;
+import 'package:hackkimura/function/audio.dart';
+import 'package:flutter_playout/player_state.dart';
 
 class Training extends StatefulWidget {
   const Training({Key? key}) : super(key: key);
@@ -13,6 +16,7 @@ class _TrainingState extends State<Training> {
   double _currentSliderValue = 60;
   bool _active = true;
   void _changeSwitch(bool e) => setState(() => _active = e);
+  void _changeMusic(bool e) => setState(() => _active = e);
 
   @override
   Widget build(BuildContext context) {
@@ -30,108 +34,143 @@ class _TrainingState extends State<Training> {
               Text(userData.chooseMode == "training" ? 'トレーニングモード' : '採点モード'),
           // 右側のアイコン一覧
         ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Padding(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                children: [
-                  Text("2Lペットボトルの上にスマホを固定し，測定開始ボタンを押してください。",
-                  style: Theme.of(context).textTheme.headline5),
-                  userData.chooseMode == "training" ? Padding(
-                    padding: EdgeInsets.only(right: 200, top: 20),
-                    child: Text("トレーニング時間",
+        body: Column(
+          children:
+          [Expanded(
+              child: SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: [
+                      Text("2Lペットボトルの上にスマホを固定し，測定開始ボタンを押してください。",
+                      style: Theme.of(context).textTheme.headline5),
+                      userData.chooseMode == "training" ? Padding(
+                        padding: EdgeInsets.only(right: 200, top: 20),
+                        child: Text("トレーニング時間",
+                                textAlign: TextAlign.left,
+                                style: Theme.of(context).textTheme.subtitle1),
+                      ) : Container(),
+                      userData.chooseMode == "training" ? Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.baseline,
+                            textBaseline: TextBaseline.alphabetic,
+                            children: [
+                              Text(_currentSliderValue.round().toString(),
+                                  textAlign: TextAlign.left,
+                                  style: Theme.of(context).textTheme.headline4),
+                              Text("秒",
+                                  textAlign: TextAlign.left,
+                                  style: Theme.of(context).textTheme.subtitle1)
+                            ],
+                          ),
+                          Container(
+                            width: size.width * 0.59,
+                            child: Slider(
+                              value: _currentSliderValue,
+                              min: 0,
+                              max: 1000,
+                              divisions: 1000,
+                              label: _currentSliderValue.round().toString(),
+                              onChanged: (double value) {
+                                setState(() {
+                                  _currentSliderValue = value;
+                                });
+                              },
+                            ),
+                          )
+                        ],
+                      ) : Container(),
+                      userData.chooseMode == "training" ? Padding(
+                        padding: EdgeInsets.only(right: 220, top: 16),
+                        child: Text("メトロノーム",
                             textAlign: TextAlign.left,
                             style: Theme.of(context).textTheme.subtitle1),
-                  ) : Container(),
-                  userData.chooseMode == "training" ? Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        textBaseline: TextBaseline.alphabetic,
-                        children: [
-                          Text(_currentSliderValue.round().toString(),
-                              textAlign: TextAlign.left,
-                              style: Theme.of(context).textTheme.headline4),
-                          Text("秒",
-                              textAlign: TextAlign.left,
-                              style: Theme.of(context).textTheme.subtitle1)
-                        ],
-                      ),
-                      Container(
-                        width: size.width * 0.59,
-                        child: Slider(
-                          value: _currentSliderValue,
-                          min: 0,
-                          max: 1000,
-                          divisions: 1000,
-                          label: _currentSliderValue.round().toString(),
-                          onChanged: (double value) {
-                            setState(() {
-                              _currentSliderValue = value;
-                            });
-                          },
+                      ) : Container(),
+                      userData.chooseMode == "training" ? Padding(
+                        padding: EdgeInsets.only(top: 8.0, left: 8.0),
+                        child: Row(
+                          children: [
+                            Text(_active ? "ON " : "OFF",
+                                textAlign: TextAlign.left,
+                                style: Theme.of(context).textTheme.headline4),
+                            Padding(
+                              padding: EdgeInsets.only(left: 8.0),
+                              child: Switch(
+                                value: _active,
+                                activeColor: Colors.green,
+                                onChanged: _changeMusic,
+                              ),
+                            ),
+                          ],
                         ),
-                      )
-                    ],
-                  ) : Container(),
-                  userData.chooseMode == "training" ? Padding(
-                    padding: EdgeInsets.only(right: 220, top: 16),
-                    child: Text("メトロノーム",
-                        textAlign: TextAlign.left,
-                        style: Theme.of(context).textTheme.subtitle1),
-                  ) : Container(),
-                  userData.chooseMode == "training" ? Padding(
-                    padding: EdgeInsets.only(top: 8.0, left: 8.0),
-                    child: Row(
-                      children: [
-                        Text(_active ? "ON " : "OFF",
-                            textAlign: TextAlign.left,
-                            style: Theme.of(context).textTheme.headline4),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8.0),
-                          child: Switch(
-                            value: _active,
-                            activeColor: Colors.green,
-                            onChanged: _changeSwitch,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ) : Container(),
+                      ) : Container(),
+                      // userData.chooseMode == "training" ? Padding(
+                      //   padding: EdgeInsets.only(right: 220, top: 16),
+                      //   child: Text("BGM",
+                      //       textAlign: TextAlign.left,
+                      //       style: Theme.of(context).textTheme.subtitle1),
+                      // ) : Container(),
+                      // userData.chooseMode == "training" ? Padding(
+                      //   padding: EdgeInsets.only(top: 8.0, left: 8.0),
+                      //   child: Row(
+                      //     children: [
+                      //       Text(_active ? "ON " : "OFF",
+                      //           textAlign: TextAlign.left,
+                      //           style: Theme.of(context).textTheme.headline4),
+                      //       Padding(
+                      //         padding: EdgeInsets.only(left: 8.0),
+                      //         child: Switch(
+                      //           value: _active,
+                      //           activeColor: Colors.green,
+                      //           onChanged: _changeSwitch,
+                      //         ),
+                      //       ),
+                      //     ],
+                      //   ),
+                      // ) : Container(),
 
-                  Center(
-                    child: Padding(
-                      padding:  userData.chooseMode == "training" ? EdgeInsets.only(top: 50) : EdgeInsets.only(top: 180),
-                      child: Container(
-                        width: 300.0,
-                        height: 300.0,
-                        child: OutlinedButton(
-                          child: Text('測定開始',
-                              style: Theme.of(context).textTheme.headline2),
-                          style: OutlinedButton.styleFrom(
-                            primary: Colors.black,
-                            shape: const StadiumBorder(),
-                            side: const BorderSide(
-                                color: Colors.green, width: 10.0),
+                      Center(
+                        child: Padding(
+                          padding:  userData.chooseMode == "training" ? EdgeInsets.only(top: 10) : EdgeInsets.only(top: 180),
+                          child: Container(
+                            width: 300.0,
+                            height: 300.0,
+                            child: OutlinedButton(
+                              child: Text('測定開始',
+                                  style: Theme.of(context).textTheme.headline2),
+                              style: OutlinedButton.styleFrom(
+                                primary: Colors.black,
+                                shape: const StadiumBorder(),
+                                side: const BorderSide(
+                                    color: Colors.green, width: 10.0),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pushNamed('/barometer',
+                                    arguments: BarometerArgs(
+                                        userData: userData,
+                                        time: _currentSliderValue,
+                                        metro: _active,
+                                        mode: userData.chooseMode));
+                              },
+                            ),
                           ),
-                          onPressed: () {
-                            Navigator.of(context).pushNamed('/barometer',
-                                arguments: BarometerArgs(
-                                    userData: userData,
-                                    time: _currentSliderValue,
-                                    metro: _active,
-                                    mode: userData.chooseMode));
-                          },
                         ),
                       ),
-                    ),
+                      // AudioPlayout(
+                      //   desiredState: PlayerState.PLAYING,
+                      // )
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
+            ),
+            AudioPlayout(
+              desiredState: PlayerState.PLAYING,
+            )],
         ));
   }
 }
